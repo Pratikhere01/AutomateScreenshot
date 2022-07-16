0ceac5d4ee6477c6acc61926ee3be10549c836f6
Pratik Ray <royshivam071@gmail.com>
2022-07-16 18:16:40 +0530

    Initial Commit

---
+++ CaptureShot.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+
+
+#To execute the path where to save screenshot
+driver = webdriver.Chrome(executable_path=r"C:\Users\roysh\OneDrive\Desktop\ss.py\chromedriver.exe")
+
+#The url that we want to take Screenshot
+driver.get("https://www.quora.com/")
+
+#Giving the file name of screenshot
+driver.save_screenshot(r"C:\Screenshot\filename.png")
